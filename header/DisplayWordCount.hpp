e9e3be2c262845bc3586722f7bb53306d40f6c0e
DJVonrhein <dvonr001@ucr.edu>
2020-12-08 22:00:45 -0800

    implemented display for no word count

---
+++ header/DisplayWordCount.hpp
@@ -0,0 +1,27 @@
+#ifndef __DISPLAY_WORD_COUNT_HPP__
+#define __DISPLAY_WORD_COUNT_HPP__
+
+#include "DisplayStrat.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include "Note.hpp"
+#include <ifstream>
+#include <cstdlib>
+using namespace std;
+
+class DisplayWordCount : class DisplayStrat {
+    public:
+	virtual void display(string title){
+	    ifstream fin;
+	    char x;
+	    fin.open(c_str("../text_files/" + title + ".txt"));
+	    while(fin >> x)
+		cout << x;
+
+	    fin.close(); 
+	}
+};
+
+#endif
